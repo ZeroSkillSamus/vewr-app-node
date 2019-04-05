3e191118f730c2560620069003e3c6d93022befe
ZeroSkillSamus <amitchell2@albany.edu>
2019-04-04 21:59:23 -0400

    fixed the css for the rightside of the page to add the placeholders for related vids

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -3,6 +3,7 @@ import ReactPlayer from 'react-player';
 import { Typography } from '@material-ui/core';
 import CommentBox from '../components/commentbox';
 import { withRouter } from 'react-router';
+import OtherVid from '../components/otherVids';
 
 var data = [
 	{ img: 'peteHunt', author: 'Pete Hunt', text: 'Yerrrr' },
@@ -15,20 +16,25 @@ var data = [
 class Video extends Component {
 	render() {
 		return (
-			<div className='videoPlayer'>
-				<div className='textBeforeVid'>
-					<Typography component='h2' variant='display2'>
-						Vewr Pre-Alpha
-					</Typography>
-					<Typography component='h2' variant='caption'>
-						Posted by Abraham Mitchell
-					</Typography>
+			<div className='allOfIT'>
+				<div className='videoPlayer'>
+					<div className='textBeforeVid'>
+						<Typography component='h2' variant='display2'>
+							Vewr Pre-Alpha
+						</Typography>
+						<Typography component='h2' variant='caption'>
+							Posted by Abraham Mitchell
+						</Typography>
+					</div>
+					<div className='video'>
+						<ReactPlayer width='900px' height='550px' controls url='https://ipfstube.erindachtler.me/ipfs/QmU1GSqu4w29Pt7EEM57Lhte8Lce6e7kuhRHo6rSNb2UaC' />
+					</div>
+					<div className='commentBox'>
+						<CommentBox data={data} />
+					</div>
 				</div>
-				<div className='video'>
-					<ReactPlayer width='900px' height='550px' controls url='https://ipfstube.erindachtler.me/ipfs/QmU1GSqu4w29Pt7EEM57Lhte8Lce6e7kuhRHo6rSNb2UaC' />
-				</div>
-				<div className='commentBox'>
-					<CommentBox data={data} />
+				<div className='videosOnSide'>
+					<OtherVid />
 				</div>
 			</div>
 		);
